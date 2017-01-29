f2139cd8eec538d18e157ad9136be4a69cf17497
Marat Shakirov <ya@maratsh.ru>
2017-01-29 14:33:27 +0300

    publish package to pypi

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='getsecure',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/maratsh/getsecure',
+    license='MIT',
+    author='maratsh',
+    author_email='ya@maratsh.ru',
+    description='Utility for securing expiring links'
+)
