3ff7dac9b0ed510cb4f886d7a68129b3a003cede
Marat Shakirov <marat.s@affinidi.com>
2024-07-24 10:30:29 +0200

    fix: package readme

--- setup.py
+++ setup.py
@@ -1,13 +1,58 @@
-from distutils.core import setup
+# coding: utf-8
 
-setup(
+"""
+setup package
+"""  # noqa: E501
+
+import setuptools
+
+setuptools.setup(
     name='getsecure',
-    version='0.0.3',
+    version='0.0.4',
     packages=[''],
     url='https://github.com/maratsh/getsecure',
     license='MIT',
     author='maratsh',
     author_email='ya@maratsh.ru',
     description='Utility for securing expiring links',
-    long_description="Utility for securing expiring links"
+    long_description="""\
+# getsecure
+
+Utility for securing expiring links
+
+## Usage
+
+### Add to nginx vhost secure location section like this
+
+```nginx
+    location /secret_page.html {
+     secure_link $arg_md5,$arg_expires;
+     secure_link_md5 "$secure_link_expires$uri <put here secret string>";
+
+     if ($secure_link = "") {
+         return 403;
+     }
+
+     if ($secure_link = "0") {
+         return 410;
+     }
+    }
+```
+
+### Get secure link
+
+```bash
+python secure.py http://example.com/secret_page.html <put here secret string> --period=30
+http://example.com/secret_page.html?md5=IQ6H6OQYGeUnlLEDgHfYNw&expires=1488221041
+```
+
+or
+
+```bash
+python secure.py /secret_page.html <put here secret string> --period=30
+/secret_page.html?md5=IQ6H6OQYGeUnlLEDgHfYNw&expires=1488221041
+```
+
+where period is days from now to future when link should work
+"""
 )
